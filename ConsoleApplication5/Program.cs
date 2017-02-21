7dc58203ac2785f7570d568a5a1ddabfd745fafa
Rajesh <Rajesh@RAJESH-PC>
2017-02-20 23:48:16 -0600

    Add project files.

---
+++ ConsoleApplication5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String content = "abcd";
+            content = content + "wxyz";
+
+            Console.WriteLine(content);
+
+            StringBuilder content2 = new StringBuilder("abcd");
+
+            content2 = content2.Append("wxyz");
+
+            Console.WriteLine(content2);
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
